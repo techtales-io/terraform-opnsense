048ae1a3820cf1f58ba6676ad4a67fa0ce1fc055
Nils Müller <tyriis@users.noreply.github.com>
2025-05-08 21:46:59 +0200

    chore(moved): cleanup migration file

--- terraform/moved.tf
+++
@@ -1,4 +0,0 @@
-moved {
-  from = module.firewall_filters["qingping-allow-mqtt"].opnsense_firewall_filter.main
-  to   = module.firewall_filters["qingping-purple-allow-mqtt"].opnsense_firewall_filter.main
-}
